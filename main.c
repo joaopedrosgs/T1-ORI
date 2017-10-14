8f23d537a3f3305188d683b95518df3d2ce97ffe
João Pedro Silva <jsangregorio@hotmail.com>
2017-10-14 18:36:18 -0300

    Insert

--- main.c
+++ main.c
@@ -4,75 +4,92 @@
 
 #define TAMANHO_REGISTRO 100
 #define TAMANHO_BLOCO 512
-#define TAMANHO_MAXIMO_BLOCO (TAMANHO_BLOCO/TAMANHO_REGISTRO)*TAMANHO_REGISTRO
 
 struct Registro {
-    char *Nome;
-    char *Sobrenome;
+  char *Nome;
+  char *Sobrenome;
 };
 
 struct Arquivo {
-    FILE *arquivo;
-    int atual;
-    int bloco_atual;
+  FILE *arquivo;
+  int atual;
+  int bloco_atual;
 };
 
-struct Arquivo CriarArquivo(char *_nome) {
-    struct Arquivo a = {fopen(_nome, "w+b"), 0,0};
-    if (a.arquivo == NULL) {
-        printf("erro");
-    }
-    return a;
+struct Arquivo CriarArquivo(char *nome) {
+  struct Arquivo a = {fopen(nome, "w+"), 0, 0};
+  if (a.arquivo == NULL) {
+    printf("Problema ao abrir o arquivo");
+  }
+  char primeiro_bloco[TAMANHO_BLOCO] = {0};
+  fwrite(primeiro_bloco, sizeof(char), TAMANHO_BLOCO, a.arquivo);
+  fflush(a.arquivo);
+  return a;
+}
+void PegarProximoBloco(char bloco[TAMANHO_BLOCO], struct Arquivo *arquivo) {
+  int lidos = fread(bloco, sizeof(char), TAMANHO_BLOCO, arquivo->arquivo);
+  if (lidos < 512) {
+    int i;
+    for (i = 0; i < TAMANHO_BLOCO; i++) bloco[i] = 0;
+  }
+  fseek(arquivo->arquivo,-lidos,SEEK_CUR);
+  fwrite(bloco,sizeof(char), TAMANHO_BLOCO,arquivo->arquivo);
 }
 
-int InserirRegistro(struct Arquivo *a, struct Registro r) {
-    int tamanho_total = strlen(r.Nome) + strlen(r.Sobrenome) + 1;
-    if (tamanho_total > TAMANHO_REGISTRO - 2) {
-        return 0;
-    }
-    if (a->atual % TAMANHO_BLOCO >= TAMANHO_MAXIMO_BLOCO) { // Se não cabe mais nenhum registro inteiro
-        a->atual=0;
-        a->bloco_atual++; // Vamos para o inicio do proximo bloco
-    }
-    char buf[tamanho_total+1];
-    snprintf(buf, sizeof buf, "%s|%s", r.Nome, r.Sobrenome);
-    
-    if (a->arquivo != NULL) {
-        int lugar =  a->atual+(a->bloco_atual)*TAMANHO_BLOCO;
-        fseek(a->arquivo, lugar, SEEK_SET);
-        fwrite(buf, tamanho_total,1, a->arquivo);
-        fflush(a->arquivo);
+int PosicaoParaInsercao(char bloco[TAMANHO_BLOCO]) {
+  int i = 0;
+  for (i = 0; i + TAMANHO_REGISTRO < TAMANHO_BLOCO; i += TAMANHO_REGISTRO) {
+    if (bloco[i] == '*' || bloco[i] == '\0') return i;
+  }
+  return -1;
+}
+void SalvarBloco(char bloco[TAMANHO_BLOCO], struct Arquivo *arquivo) {
+  fseek(arquivo->arquivo, -TAMANHO_BLOCO, SEEK_CUR);
+  fwrite(bloco, sizeof(char), TAMANHO_BLOCO, arquivo->arquivo);
+}
+int InserirRegistro(struct Arquivo *arquivo, struct Registro r) {
+  int res = 0;
+  int tamanho_total = strlen(r.Nome) + strlen(r.Sobrenome) + 1;
+  if (tamanho_total > TAMANHO_REGISTRO - 2) {
+    return res;
+  }
+  rewind(arquivo->arquivo);
 
-    } else {
-        printf("Arquivo não aberto");
-    }
-    a->atual = a->atual+100;
+  char bloco[TAMANHO_BLOCO] = {0};
+  int posicao = -1;
+  while (posicao < 0) {
+    PegarProximoBloco(bloco, arquivo);
+    posicao = PosicaoParaInsercao(bloco);
+  };
+
+  char buf[tamanho_total + 1];
+  snprintf(buf, sizeof buf, "%s|%s", r.Nome, r.Sobrenome);
+  int i = 0;
+  for (i = 0; i < tamanho_total; i++) {
+    bloco[posicao + i] = buf[i];
+  }
+  res = 1;
+  SalvarBloco(bloco, arquivo);
+  return res;
 }
 
-
-void main() {
-    struct Arquivo b = CriarArquivo("alaao");
-    if(b.arquivo==NULL) {
-        printf("erro");
+int main() {
+  struct Arquivo b = CriarArquivo("alaao");
+  if (b.arquivo == NULL) {
+    printf("erro");
+  }
+  struct Registro a[] = {
+      {"Joao Pedro", "Sao gregorio Silva"}, {"Kaua", "Ferreira Costa"},
+      {"Lara", "Goncalves Cavalcanti"},     {"Vitória", "Gomes Barros"},
+      {"Kaua", "Ferreira Costa"},           {"Lara", "Goncalves Cavalcanti"},
+      {"Vitória", "Gomes Barros"}};
+  int i = 0;
+  int sizeA = sizeof(a) / sizeof(a[0]);
+  for (i = 0; i < sizeA; i++) {
+    if (!InserirRegistro(&b, a[i])) {
+      printf("Erro ao inserir");
     }
-    struct Registro a[] = {{"Joao Pedro", "Sao gregorio Silva"},
-                            {"Joao Pedro", "Sao gregorio Silva"},
-                            {"Joao Pedro", "Sao gregorio Silva"},
-                            {"Joao Pedro", "Sao gregorio Silva"},
-                            {"Joaasdasddro", "Saaddorio Silva"},
-                            {"Joao Pedro", "Sgorva"},
-                            {"Joo", "Sao gregorio Silva"},
-                            {"Joao Pedro", "Sao gva"},
-                            {"Joao Pesasdasdasdsadasdsaddro", "Sao gregorio Silva"},
-                            {"Joao Pedro", "Sao gregoasdasdrio Silva"},
-                            {"Joao Pedro", "Sao gregorasdasdadio Silva"},
-                            {"Joao Pedro", "Sao gregorio Sasdasdilva"}};
-    int i=0;
-    int sizeA = sizeof(a)/sizeof(a[0]);
-   for(i=0; i<sizeA;i++) {
-    InserirRegistro(&b, a[i]);
-    
-   }
-    fclose(b.arquivo);
-
+  }
+  fclose(b.arquivo);
+  return 0;
 }
